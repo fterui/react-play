670cd3c6bdeec33ca480f8c31c0615b97e0b80bd
Fumihiko Terui <25654992+fterui@users.noreply.github.com>
2022-08-30 08:18:53 +0900

    Sign in with Google

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from 'react';
+import jwtDecode from "jwt-decode";
+
+const LoginButton = () => {
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "53370007102-gasokudqj0tb0s3kup5l7ak0uroehrl8.apps.googleusercontent.com",
+      callback: (res) => setUser(jwtDecode(res.credential))
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("buttonDiv"),
+      {theme: "outline", size:"large"}
+    )
+  }, []);
+  return <>
+    <div id="buttonDiv" hidden={!!user.name}/>
+    {user.name && <><div>Hello {user.name}!</div><button onClick={() => setUser({})}>Log out</button></>}
+  </>
+}
+
 
 function App() {
   const [text, setText] = useState("");
@@ -12,6 +33,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <LoginButton/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {text}
